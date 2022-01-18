014a77dacccf7e927d4384003b14af44fb2ffcee
NgTrungThang <93510689+NgTrungThang@users.noreply.github.com>
2022-01-18 14:26:47 +0700

    Delete test1.py

--- test1.py
+++
@@ -1,6 +0,0 @@
-def test_nhi_phan(a):
-    if(a == '0000' or a== '0001' or a == '0010' or a== '0011' or a == '0100' or a== '0101' or 
-    a == '0110' or a== '0111' or  a == '1001'):
-        return True
-    else:
-        return False
